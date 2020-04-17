266c90a738a82dca1f6c5cf653a0ff49ccf2848f
Fernando Pinheiro <fnandogp@gmail.com>
2020-04-17 14:30:59 +1000

    remove console.log

--- src/typeorm-config.service.ts
+++ src/typeorm-config.service.ts
@@ -7,13 +7,6 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    console.log(
-      'this.configService.database.name',
-      this.configService.get<string>('database.name'),
-      'this.configService.database.host',
-      this.configService.get<string>('database.host'),
-    );
-
     return {
       type: 'mongodb',
       host: this.configService.get<string>('database.host'),
